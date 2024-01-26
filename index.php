59b1bd61146be32361961f57467c76f73840d80c
Daniel Penelva <d4n.andrade@gmail.com>
2024-01-26 01:52:55 -0300

    Exemplos de estrutura de repetição

--- index.php
+++ index.php
@@ -2,67 +2,74 @@
 
 include 'Helpers.php';
 
-/* MATCH  - A estrutura de controle match foi introduzida no PHP 8.0 para facilitar comparações de valores em uma expressão mais concisa e semelhante a um switch.*/
+// Exemplos de For
 
-// Exemplo Básico 1
+for ($i = 1; $i <= 5; $i++) {
+    echo $i . " ";
+} // Saída: 1 2 3 4 5
 
-$diaDaSemana = 3;
-
-$resultado = match ($diaDaSemana) {
-    1 => "Domingo",
-    2 => "Segunda-feira",
-    3 => "Terça-feira",
-    4 => "Quarta-feira",
-    5 => "Quinta-feira",
-    6 => "Sexta-feira",
-    7 => "Sábado",
-    default => "Dia inválido",
-};
-
-echo $resultado;
-
-echo '<br />';
-
-// Exemplo Básico 2 - do tipo string
-$diaDaSemana = date('w');
-
-$resultado = match ($diaDaSemana) {
-    '1' => "Domingo",
-    '2' => "Segunda-feira",
-    '3' => "Terça-feira",
-    '4' => "Quarta-feira",
-    '5' => "Quinta-feira",
-    '6' => "Sexta-feira",
-    '7' => "Sábado",
-    default => "Dia inválido",
-};
-
-echo $resultado;
 
 echo '<hr />';
 
-// Exemplo com Condições Adicionais
-
-$tipoAnimal = "gato";
-
-$resultado = match ($tipoAnimal) {
-    "cachorro", "gato" => "Animal doméstico",
-    "leão", "tigre", "elefante" => "Animal selvagem",
-    default => "Tipo de animal desconhecido",
-};
-
-echo $resultado;
+for($y = 1; $y <= 10; $y++){
+    echo $y % 2 ? $y . ' é impar' : $y . ' é par' . '<br />';
+}
 
 echo '<hr />';
 
-// Exemplo com Expressões Mais Complexas
+$numero = 5;
 
-$numero = 2;
+for($n = 1; $n <= 10; $n++){
+    echo $n . 'x' . $numero . ' = ' . $n*$numero . '<br />';
+}
 
-$resultado = match (true) {
-    $numero >= 1 && $numero <= 3 => "O número é 1, 2 ou 3",
-    $numero == 4 => "O número é 4",
-    default => "Número desconhecido",
-};
+echo '<hr />';
 
-echo $resultado;
+// Exemplo while
+
+$i = 1;
+
+while ($i <= 5) {
+    echo $i . " ";
+    $i++;
+}
+// Saída: 1 2 3 4 5
+
+echo '<hr />';
+
+// Exemplo do...while()
+
+$i = 1;
+
+do {
+    echo $i . " ";
+    $i++;
+} while ($i <= 5);
+// Saída: 1 2 3 4 5
+
+echo '<hr />';
+
+// Exemplo continue
+
+for ($i = 1; $i <= 5; $i++) {
+    if ($i == 3) {
+        continue; // Pula a iteração quando $i é igual a 3
+    }
+    echo $i . " ";
+}
+// Saída: 1 2 4 5
+
+echo '<hr />';
+
+// Exemplo break
+$i = 1;
+
+while ($i <= 5) {
+    if ($i == 4) {
+        break; // Sai do loop quando $i é igual a 4
+    }
+    echo $i . " ";
+    $i++;
+}
+// Saída: 1 2 3
+
